eb5379e2ea6b598013cc371456b3121ef4615ca7
Tiago Matos <tiagoms88@gmail.com>
2024-01-18 21:54:11 -0300

    two more unit tests

--- test/estruturasTests.cpp
+++ test/estruturasTests.cpp
@@ -12,14 +12,16 @@ class Testes_estruturas : public CppUnit::TestFixture
   CPPUNIT_TEST( test_novoComponente );
   CPPUNIT_TEST( test_novaListaComponente );
   CPPUNIT_TEST( test_novaListaComponenteTamanho );
+  CPPUNIT_TEST( test_novoCircuito );
+  CPPUNIT_TEST( test_insereComponente );
   CPPUNIT_TEST_SUITE_END();
 
 public:
 
   void test_novoComponente()
   {
-	char nome[] = "entrada_0"; 
-	Componente c0 = novoComponente( (char*)nome, input );
+    char nome[] = "entrada_0"; 
+    Componente c0 = novoComponente( (char*)nome, input );
 
     CPPUNIT_ASSERT(c0);
     CPPUNIT_ASSERT( !strcmp( (char*)(c0->nome), (char*)nome ) );
@@ -31,20 +33,70 @@ public:
 
   void test_novaListaComponente()
   {
-	ListaComponente* list = novaListaComponente();
-	
-	CPPUNIT_ASSERT(list);
-	CPPUNIT_ASSERT_EQUAL(list->tamanho, 0);
-	CPPUNIT_ASSERT(!list->itens);
+    ListaComponente* list = novaListaComponente();
+
+    CPPUNIT_ASSERT(list);
+    CPPUNIT_ASSERT_EQUAL(list->tamanho, 0);
+    CPPUNIT_ASSERT(!list->itens);
   }
 
   void test_novaListaComponenteTamanho()
   {
     ListaComponente* list = novaListaComponenteTamanho(50);
-	
+
     CPPUNIT_ASSERT(list);
     CPPUNIT_ASSERT_EQUAL(list->tamanho, 50);
     CPPUNIT_ASSERT(list->itens);
+  }
+
+  void test_novoCircuito()
+  {
+    t_circuito* circ = novoCircuito();
+
+    CPPUNIT_ASSERT(circ);
+
+    CPPUNIT_ASSERT(circ->listaFiosEntrada);
+    CPPUNIT_ASSERT_EQUAL(circ->listaFiosEntrada->tamanho, 0);
+    CPPUNIT_ASSERT(!circ->listaFiosEntrada->itens);
+
+    CPPUNIT_ASSERT(circ->listaFiosSaida);
+    CPPUNIT_ASSERT_EQUAL(circ->listaFiosSaida->tamanho, 0);
+    CPPUNIT_ASSERT(!circ->listaFiosSaida->itens);
+
+    CPPUNIT_ASSERT(circ->listaWires);
+    CPPUNIT_ASSERT_EQUAL(circ->listaWires->tamanho, 0);
+    CPPUNIT_ASSERT(!circ->listaWires->itens);
+
+    CPPUNIT_ASSERT(circ->listaPortas);
+    CPPUNIT_ASSERT_EQUAL(circ->listaPortas->tamanho, 0);
+    CPPUNIT_ASSERT(!circ->listaPortas->itens);
+  }
+
+  void test_insereComponente()
+  {
+    char str_c0[] = "entrada_0";
+    char str_c1[] = "entrada_1"; 
+    Componente c0 = novoComponente( (char*)str_c0, input );
+    Componente c1 = novoComponente( (char*)str_c1, input );
+
+    ListaComponente* list = novaListaComponente();
+
+    CPPUNIT_ASSERT(c0);
+    CPPUNIT_ASSERT(c1);
+    CPPUNIT_ASSERT(list);
+    CPPUNIT_ASSERT_EQUAL(list->tamanho, 0);
+
+    insereComponente(list, c0);
+
+    CPPUNIT_ASSERT_EQUAL(list->tamanho, 1);
+    CPPUNIT_ASSERT(list->itens);
+    CPPUNIT_ASSERT_EQUAL(list->itens[0], c0);
+
+    insereComponente(list, c1);
+
+    CPPUNIT_ASSERT_EQUAL(list->tamanho, 2);
+    CPPUNIT_ASSERT(list->itens);
+    CPPUNIT_ASSERT_EQUAL(list->itens[1], c1);
   }
 
 };
--- test/eventosTests.cpp
+++ test/eventosTests.cpp
@@ -18,14 +18,14 @@ public:
 
   void test_insereEvento()
   {
-	Evento *fila = NULL;
+    Evento *fila = NULL;
 
-	Tempo t = (Tempo)5000;
-	Componente c0 = novoComponente( (char*)"entrada_componente_0", input );
-	ValorLogico v = um;
+    Tempo t = (Tempo)5000;
+    Componente c0 = novoComponente( (char*)"entrada_componente_0", input );
+    ValorLogico v = um;
 
-	insereEvento( &fila, t, c0, v);
-	
+    insereEvento( &fila, t, c0, v);
+  
     CPPUNIT_ASSERT(fila);
     CPPUNIT_ASSERT_EQUAL(fila->quando, t);
     CPPUNIT_ASSERT(fila->listaTransicao);
@@ -34,20 +34,18 @@ public:
     CPPUNIT_ASSERT(!fila->listaTransicao->proximo);
     CPPUNIT_ASSERT(!fila->proximo);
 
-	t = 11000;
+    t = 11000;
     v = zero;
-	Componente c1 = novoComponente( (char*)"entrada_componente_1", input );
+    Componente c1 = novoComponente( (char*)"entrada_componente_1", input );
 
-	insereEvento( &fila, t, c1, v);
+    insereEvento( &fila, t, c1, v);
 
-	CPPUNIT_ASSERT(fila);
+    CPPUNIT_ASSERT(fila);
     CPPUNIT_ASSERT_EQUAL(fila->quando, (Tempo)5000);
-	CPPUNIT_ASSERT(fila->listaTransicao);
-	CPPUNIT_ASSERT_EQUAL(fila->listaTransicao->fio, c0);	
+    CPPUNIT_ASSERT(fila->listaTransicao);
+    CPPUNIT_ASSERT_EQUAL(fila->listaTransicao->fio, c0);
 
-	//TODO: terminar e testar as outras possibilidades
-
+    //TODO: terminar e testar as outras possibilidades
   }
 
 };
-
