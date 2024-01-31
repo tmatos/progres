ec244c1690c7c4f9d6ec54b4413ff08f5328ff1b
Tiago Matos <tiagoms88@gmail.com>
2024-01-31 13:39:02 -0300

    more assertons in test case

--- test/verilogTests.cpp
+++ test/verilogTests.cpp
@@ -17,6 +17,18 @@ public:
   {
     CPPUNIT_ASSERT( isPortaLogica( (char*)"and") );
     CPPUNIT_ASSERT( isPortaLogica( (char*)"or") );
+    CPPUNIT_ASSERT( isPortaLogica( (char*)"xor") );
+    CPPUNIT_ASSERT( isPortaLogica( (char*)"nand") );
+    CPPUNIT_ASSERT( isPortaLogica( (char*)"nor") );
+    CPPUNIT_ASSERT( isPortaLogica( (char*)"xnor") );
+    CPPUNIT_ASSERT( isPortaLogica( (char*)"not") );
+    CPPUNIT_ASSERT( isPortaLogica( (char*)"buf") );
+    CPPUNIT_ASSERT( ! isPortaLogica( (char*)"a") );
+    CPPUNIT_ASSERT( ! isPortaLogica( (char*)"aa") );
+    CPPUNIT_ASSERT( ! isPortaLogica( (char*)"n") );
+    CPPUNIT_ASSERT( ! isPortaLogica( (char*)"nn") );
+    CPPUNIT_ASSERT( ! isPortaLogica( (char*)"") );
+    CPPUNIT_ASSERT( ! isPortaLogica( (char*)" ") );
   }
 
 };
