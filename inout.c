7d8b14a7bd0cef111f104b1b89a917fe88f6a58d
Tiago Matos <tiagoms88@gmail.com>
2025-04-06 20:39:52 -0300

    refactoring: better code style

--- inout.c
+++ inout.c
@@ -7,19 +7,25 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-//#include <ctype.h>
 
 #include "sinais.h"
 #include "lex.h"
 #include "inout.h"
 
-Sinais* carregaEntradas(FILE *arquivo) {
+Sinais* erroFatalArquivoCorrompido()
+{
+    printf(MSG_ARQUIVO_ENTRADA_CORROMPIDO);
+    return NULL;
+}
+
+Sinais* carregaEntradas(FILE* arquivo)
+{
     int indice = -1; // indexador do vetor de sinais de entrada
     ValorLogico valorLogico;
-    Sinais *entradas = novaSinais();
-    Token *it = NULL;
+    Sinais* entradas = novaSinais();
+    Token* it = NULL;
 
-    ListaToken* nomesUsados = novaListaToken(); // nomes de entrada já lidos
+    ListaToken* nomesUsados = novaListaToken(); // nomes de entrada ja lidos
 
     ListaToken* tokens = tokeniza(arquivo);
 
@@ -29,15 +35,14 @@ Sinais* carregaEntradas(FILE *arquivo) {
     it = tokens->primeiro;
 
     if(!it) {
-        printf("Arquivo de entrada aparentemente vazio.\n");
+        printf(MSG_ARQUIVO_ENTRADA_VAZIO);
         return NULL;
     }
 
-    while(1) {
-        if( isSimbolo( it->valor[0] ) ) {
-            printf(MSG_ARQUIVO_ENTRADA_CORROMPIDO);
-            return NULL;
-        }
+    while(1)
+    {
+        if( isSimbolo( it->valor[0] ) )
+            return erroFatalArquivoCorrompido();
 
         insereTokenString(nomesUsados, it->valor, it->linha, it->coluna);
 
@@ -46,103 +51,80 @@ Sinais* carregaEntradas(FILE *arquivo) {
 
         avanca(&it);
 
-        if(!it) {
-            printf(MSG_ARQUIVO_ENTRADA_CORROMPIDO);
-            return NULL;
-        }
+        if(!it)
+            return erroFatalArquivoCorrompido();
 
-        if( iguais(it->valor, "{") ) {
-            // loop para um sinal
-            while(1) {
-                avanca(&it);
+        if( !iguais(it->valor, "{") )
+            return erroFatalArquivoCorrompido();
 
-                if(!it) {
-                    printf(MSG_ARQUIVO_ENTRADA_CORROMPIDO);
-                    return NULL;
-                }
+        // loop para um sinal
+        while(1)
+        {
+            avanca(&it);
 
-                valorLogico = nulo;
+            if(!it)
+                return erroFatalArquivoCorrompido();
 
-                if(iguais(it->valor, "0")) {
-                    valorLogico = zero;
-                }
-                else if(iguais(it->valor, "1")) {
-                    valorLogico = um;
-                }
-                else if(iguais(it->valor, "x") || iguais(it->valor, "X")) {
-                    valorLogico = x;
-                }
-                else if(iguais(it->valor, "}")){
-                    break;
-                }
-                else {
-                    printf(MSG_ARQUIVO_ENTRADA_CORROMPIDO);
-                    return NULL;
-                }
+            valorLogico = nulo;
 
-                avanca(&it);
-
-                if(!it) {
-                    printf(MSG_ARQUIVO_ENTRADA_CORROMPIDO);
-                    return NULL;
-                }
-
-                if(iguais(it->valor, "(")) {
-                    avanca(&it);
-
-                    if(!it) {
-                        printf(MSG_ARQUIVO_ENTRADA_CORROMPIDO);
-                        return NULL;
-                    }
-
-                    if(isNumNaturalValido(it->valor)) {
-                        addPulso(entradas->lista + indice, valorLogico, atoi(it->valor));
-                    }
-                    else {
-                        printf(MSG_ARQUIVO_ENTRADA_CORROMPIDO);
-                        return NULL;
-                    }
-
-                    avanca(&it);
-
-                    if(!it) {
-                        printf(MSG_ARQUIVO_ENTRADA_CORROMPIDO);
-                        return NULL;
-                    }
-
-                    if( iguais(it->valor, ")") ) {
-                        avanca(&it);
-
-                        if(!it) {
-                            printf(MSG_ARQUIVO_ENTRADA_CORROMPIDO);
-                            return NULL;
-                        }
-
-                        if( iguais(it->valor, ",") )
-                            continue;
-                        else if( iguais(it->valor, "}") )
-                            break;
-                    }
-                    else {
-                        printf(MSG_ARQUIVO_ENTRADA_CORROMPIDO);
-                        return NULL;
-                    }
-                }
-                else {
-                    printf(MSG_ARQUIVO_ENTRADA_CORROMPIDO);
-                    return NULL;
-                }
+            if( iguais(it->valor, "0") ) {
+                valorLogico = zero;
             }
-        }
-        else {
-            printf(MSG_ARQUIVO_ENTRADA_CORROMPIDO);
-            return NULL;
+            else if( iguais(it->valor, "1") ) {
+                valorLogico = um;
+            }
+            else if( iguais(it->valor, "x") || iguais(it->valor, "X") ) {
+                valorLogico = x;
+            }
+            else if( iguais(it->valor, "}") ) {
+                break;
+            }
+            else {
+                return erroFatalArquivoCorrompido();
+            }
+
+            avanca(&it);
+
+            if(!it)
+                return erroFatalArquivoCorrompido();
+
+            if( !iguais(it->valor, "(") )
+                return erroFatalArquivoCorrompido();
+
+            avanca(&it);
+
+            if(!it)
+                return erroFatalArquivoCorrompido();
+
+            if( !isNumNaturalValido(it->valor) )
+                return erroFatalArquivoCorrompido();
+            
+            addPulso(entradas->lista + indice, valorLogico, atoi(it->valor));
+
+            avanca(&it);
+
+            if(!it)
+                return erroFatalArquivoCorrompido();
+
+            if( !iguais(it->valor, ")") )
+                return erroFatalArquivoCorrompido();
+
+            avanca(&it);
+
+            if(!it)
+                return erroFatalArquivoCorrompido();
+
+            if( iguais(it->valor, ",") )
+                continue;
+            else if( iguais(it->valor, "}") )
+                break;
+             
         }
 
         avanca(&it);
 
         if(!it) {
-            printf("Arquivo de entrada completamente lido.\n");
+            printf(MSG_ARQUIVO_ENTRADA_LIDO_COMPLETO);
             break;
         }
     }
@@ -150,11 +132,11 @@ Sinais* carregaEntradas(FILE *arquivo) {
     return entradas;
 }
 
-void salvarSinais(Sinais *sinaisSaida, FILE *arqSaida)
+void salvarSinais(Sinais* sinaisSaida, FILE* arqSaida)
 {
     int si; // indexador dos sinais na lista de sinais de entrada
-    Sinal *itSinais = NULL; // Iterador para os sinais num conjunto de entrada ou saida
-    Pulso *it = NULL; // Iterador para os pulsos em um Sinal
+    Sinal* itSinais = NULL; // Iterador para os sinais num conjunto de entrada ou saida
+    Pulso* it = NULL; // Iterador para os pulsos em um Sinal
 
     if(!sinaisSaida || !arqSaida)
         return;
@@ -162,15 +144,16 @@ void salvarSinais(Sinais *sinaisSaida, FILE *arqSaida)
     si = 0;
     itSinais = sinaisSaida->lista;
 
-    while(si < sinaisSaida->quantidade)
+    while( si < sinaisSaida->quantidade )
     {
         fprintf(arqSaida, "%s {", itSinais[si].nome);
 
         it = itSinais[si].pulsos; // Aqui, o indice 0 indica qual dos sinais na lista
 
-        while(it->valor != nulo)
+        while( it->valor != nulo )
         {
-            if(it != itSinais[si].pulsos) // Insere virgula apenas se não é a primeira iteração
+            // Insere virgula apenas se nao eh a primeira iteracao
+            if( it != itSinais[si].pulsos )
                 fprintf(arqSaida, ", ");
 
             switch(it->valor)
--- inout.h
+++ inout.h
@@ -7,18 +7,31 @@
 
 #define INOUT_H
 
-#define MSG_ARQUIVO_ENTRADA_CORROMPIDO "Arquivo de entrada corrompido.\n" /// Msg a ser impressa em caso de erros no arquivo de ondas
+/// Mensagem a ser impressa em caso de erros no arquivo de ondas
+#define MSG_ARQUIVO_ENTRADA_CORROMPIDO "Arquivo de entrada corrompido.\n"
+
+/// Mensagem a ser impressa no caso de o arquivo estar vazio
+#define MSG_ARQUIVO_ENTRADA_VAZIO "Arquivo de entrada aparentemente vazio.\n"
+
+/// Mensagem a ser impressa quando o arquivo for lido completamente
+#define MSG_ARQUIVO_ENTRADA_LIDO_COMPLETO "Arquivo de entrada completamente lido.\n"
 
 /** @brief Cria uma estrutura de dados representando todos os sinas de entrada
-            lidos partir do arquivo de entrada correspondente (extensão *.in).
+           lidos partir do arquivo de entrada correspondente (extensão *.in).
  *  @param arquivo O handler do arquivo de entrada com sinais a ser processado.
  *  @return A estrutura de dados contendo todos os sinais lidos do arquivo.
  */
-Sinais* carregaEntradas(FILE *arquivo);
+Sinais* carregaEntradas(FILE* arquivo);
 
-/** @brief Salva todos os sinais contidos no conjunto para o arquivo de saida
-            com a formatação padrão.
+/** @brief Salva todos os sinais contidos no conjunto para o arquivo de saída
+           com a formatação padrão.
+ *  @param sinaisSaida Conjunto dos sinais a serem salvos.
+ *  @param arqSaida Arquivo a ser escrito com a representação dos sinais.
  */
-void salvarSinais(Sinais *sinaisSaida, FILE *arqSaida);
+void salvarSinais(Sinais* sinaisSaida, FILE* arqSaida);
+
+/** @brief Exibe uma mensagem de erro e retorna um NULL.
+*/
+Sinais* erroFatalArquivoCorrompido();
 
 #endif // INOUT_H
