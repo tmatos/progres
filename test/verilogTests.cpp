83740761a582e48279bc78ae00bbc4e5febd70de
Tiago Matos <tiagoms88@gmail.com>
2025-04-07 01:29:55 -0300

    add unit tests for incorrect verilog code detection

--- test/verilogTests.cpp
+++ test/verilogTests.cpp
@@ -2,6 +2,8 @@
 #include <cppunit/ui/text/TestRunner.h>
 #include <cppunit/TextOutputter.h>
 #include <cstring>
+#include <list>
+#include <string>
 
 #include "../verilog.h"
 
@@ -12,6 +14,7 @@ class Testes_verilog : public CppUnit::TestFixture
   CPPUNIT_TEST( test_carregaCircuito_fileEmpty );
   CPPUNIT_TEST( test_carregaCircuito_fileTop_module );
   CPPUNIT_TEST( test_carregaCircuito_fileTudo_module );
+  CPPUNIT_TEST( test_carregaCircuito_badverilog_XX_v );
   CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -36,38 +39,60 @@ public:
 
   void test_carregaCircuito_fileEmpty()
   {
-    t_circuito *circuit = NULL;
-    FILE *arquivoVerilogVazio = fopen("./verilog_sample_src/empty.v", "r");
-
+    t_circuito* circuit = NULL;
+    FILE* arquivoVerilogVazio = fopen("./verilog_sample_src/empty.v", "r");
     CPPUNIT_ASSERT( arquivoVerilogVazio );
-
     circuit = carregaCircuito(arquivoVerilogVazio);
-
-    CPPUNIT_ASSERT( ! circuit );
+    CPPUNIT_ASSERT( !circuit );
+    fclose(arquivoVerilogVazio);
   }
 
   void test_carregaCircuito_fileTop_module()
   {
-    t_circuito *circuit = NULL;
-    FILE *arquivoVerilogTop = fopen("./verilog_sample_src/top.v", "r");
-
+    t_circuito* circuit = NULL;
+    FILE* arquivoVerilogTop = fopen("./verilog_sample_src/top.v", "r");
     CPPUNIT_ASSERT( arquivoVerilogTop );
-
     circuit = carregaCircuito(arquivoVerilogTop);
-
     CPPUNIT_ASSERT( circuit );
+    fclose(arquivoVerilogTop);
   }
 
   void test_carregaCircuito_fileTudo_module()
   {
     t_circuito* circuit = NULL;
     FILE* arquivo = fopen("./verilog_sample_src/tudo.v", "r");
-
     CPPUNIT_ASSERT( arquivo );
-
     circuit = carregaCircuito(arquivo);
-
     CPPUNIT_ASSERT( circuit );
+    fclose(arquivo);
+  }
+
+  void test_carregaCircuito_badverilog_XX_v()
+  {
+    std::list<std::string> list_bad_files = {
+      "./verilog_sample_src/badverilog_00.v",
+      "./verilog_sample_src/badverilog_01.v",
+      "./verilog_sample_src/badverilog_02.v",
+      "./verilog_sample_src/badverilog_03.v",
+      "./verilog_sample_src/badverilog_04.v",
+      "./verilog_sample_src/badverilog_05.v",
+      "./verilog_sample_src/badverilog_06.v",
+      "./verilog_sample_src/badverilog_07.v",
+      "./verilog_sample_src/badverilog_08.v",
+      "./verilog_sample_src/badverilog_09.v"
+    };
+
+    t_circuito* circuit = NULL;
+    FILE* fp = NULL;
+
+    for ( std::string path : list_bad_files )
+    {
+      fp = fopen( path.c_str(), "r");
+      CPPUNIT_ASSERT( fp );
+      circuit = carregaCircuito(fp);
+      CPPUNIT_ASSERT( !circuit );
+      fclose(fp);
+    }
   }
 
 };
