1ad10ce9c22a2bd73afc9ac2c4e21af2a06d7d38
Tiago Matos <tiagoms88@gmail.com>
2025-04-07 18:39:07 -0300

    improve test coverage

--- test/estruturasTests.cpp
+++ test/estruturasTests.cpp
@@ -14,6 +14,7 @@ class Testes_estruturas : public CppUnit::TestFixture
   CPPUNIT_TEST( test_novaListaComponenteTamanho );
   CPPUNIT_TEST( test_novoCircuito );
   CPPUNIT_TEST( test_insereComponente );
+  CPPUNIT_TEST( test_getXPorNome );
   CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -22,7 +23,6 @@ public:
   {
     char nome[] = "entrada_0"; 
     Componente c0 = novoComponente( (char*)nome, input );
-
     CPPUNIT_ASSERT(c0);
     CPPUNIT_ASSERT( !strcmp( (char*)(c0->nome), (char*)nome ) );
     CPPUNIT_ASSERT_EQUAL( c0->tipo.operador, input );
@@ -34,7 +34,6 @@ public:
   void test_novaListaComponente()
   {
     ListaComponente* list = novaListaComponente();
-
     CPPUNIT_ASSERT(list);
     CPPUNIT_ASSERT_EQUAL(list->tamanho, 0);
     CPPUNIT_ASSERT(!list->itens);
@@ -43,7 +42,6 @@ public:
   void test_novaListaComponenteTamanho()
   {
     ListaComponente* list = novaListaComponenteTamanho(50);
-
     CPPUNIT_ASSERT(list);
     CPPUNIT_ASSERT_EQUAL(list->tamanho, 50);
     CPPUNIT_ASSERT(list->itens);
@@ -52,7 +50,6 @@ public:
   void test_novoCircuito()
   {
     t_circuito* circ = novoCircuito();
-
     CPPUNIT_ASSERT(circ);
 
     CPPUNIT_ASSERT(circ->listaFiosEntrada);
@@ -87,16 +84,50 @@ public:
     CPPUNIT_ASSERT_EQUAL(list->tamanho, 0);
 
     insereComponente(list, c0);
-
     CPPUNIT_ASSERT_EQUAL(list->tamanho, 1);
     CPPUNIT_ASSERT(list->itens);
     CPPUNIT_ASSERT_EQUAL(list->itens[0], c0);
 
     insereComponente(list, c1);
-
     CPPUNIT_ASSERT_EQUAL(list->tamanho, 2);
     CPPUNIT_ASSERT(list->itens);
     CPPUNIT_ASSERT_EQUAL(list->itens[1], c1);
+  }
+
+  void test_getXPorNome()
+  {
+    char* null_name = NULL;
+    t_circuito* circ = novoCircuito();
+    CPPUNIT_ASSERT(circ);    
+
+    CPPUNIT_ASSERT( !getPortaPorNome(circ, null_name) );
+    CPPUNIT_ASSERT( !getWirePorNome(circ, null_name) );
+    CPPUNIT_ASSERT( !getInputPorNome(circ, null_name) );
+    CPPUNIT_ASSERT( !getOutputPorNome(circ, null_name) );
+
+    char str_porta[] = "porta_01";
+    char str_wire[] = "fio_01"; 
+    char str_in[] = "entrada_01";
+    char str_out[] = "saida_01"; 
+    Componente c_porta = novoComponente( (char*)str_porta, op_and );
+    Componente c_wire = novoComponente( (char*)str_wire, wire );
+    Componente c_in = novoComponente( (char*)str_in, input );
+    Componente c_out = novoComponente( (char*)str_out, output );
+
+    insereComponente(circ->listaPortas, c_porta);
+    insereComponente(circ->listaWires, c_wire);
+    insereComponente(circ->listaFiosEntrada, c_in);
+    insereComponente(circ->listaFiosSaida, c_out);
+
+    CPPUNIT_ASSERT( getPortaPorNome(circ, str_porta) );
+    CPPUNIT_ASSERT( getWirePorNome(circ, str_wire) );
+    CPPUNIT_ASSERT( getInputPorNome(circ, str_in) );
+    CPPUNIT_ASSERT( getOutputPorNome(circ, str_out) );
+
+    CPPUNIT_ASSERT( !getPortaPorNome(circ, str_in) );
+    CPPUNIT_ASSERT( !getWirePorNome(circ, str_in) );
+    CPPUNIT_ASSERT( !getInputPorNome(circ, str_out) );
+    CPPUNIT_ASSERT( !getOutputPorNome(circ, str_in) );
   }
 
 };
