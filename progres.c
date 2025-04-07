d0c5d8117745b1ed12a748fcfb207d87d5c013b2
Tiago Matos <tiagoms88@gmail.com>
2025-04-06 21:48:14 -0300

    improve code style even more

--- progres.c
+++ progres.c
@@ -18,89 +18,97 @@
 
 int main(int argc, char* argv[])
 {
-    FILE* arquivoVerilog = NULL;
-    FILE* wavein = NULL;
-    FILE* waveout = NULL;
+    FILE* f_verilog_source = NULL;
+    FILE* f_wave_in = NULL;
+    FILE* f_wave_out = NULL;
 
     Sinais* entradas = NULL;
     Sinais* saidas = NULL;
-
     t_circuito* circuto1 = NULL;
+
+    char str_wave_out_filepath[MAX_FILE_PATH_SIZE] = "";
 
     if(argc < 2) {
         printf("Uso: progres [arquivo verilog] [arquivo de entradas]\n");
         exit(0);
     }
 
-    arquivoVerilog = fopen(argv[1], "r");
+    f_verilog_source = fopen(argv[1], "r");
 
-    if(!arquivoVerilog) {
+    if(!f_verilog_source) {
         printf("Impossibilitado de abrir o arquivo: %s\n", argv[1]);
         exit(1);
     }
 
     printf("Abrindo o arquivo de circuito: %s\n", argv[1]);
 
-    circuto1 = carregaCircuito(arquivoVerilog);
+    circuto1 = carregaCircuito(f_verilog_source);
 
-    fclose(arquivoVerilog);
+    fclose(f_verilog_source);
 
     if(circuto1) {
         printf("Circuito carregado com sucesso.\n");
     }
     else {
-        printf("Erro com o codigo fonte do cicuito.\n");
+        printf("Erro com o carregamento do codigo fonte do cicuito.\n");
+        exit(1);
     }
 
-    // se foi fornecido um path do arquivo de entrada para simulacao, simularemos
-    if(argc > 2) {
-        wavein = fopen(argv[2], "r");
-
-        if(!wavein) {
-            printf("Impossibilitado de abrir o arquivo de entrada: %s\n", argv[2]);
-            exit(1);
-        }
-
-        printf("Abrindo o arquivo de entrada: %s\n", argv[2]);
-
-        entradas = carregaEntradas(wavein);
-
-        fclose(wavein);
-
-        if(entradas) {
-            char pathArquivoSaida[MAX_FILE_PATH_SIZE] = "";
-
-            if(argc > 3) { // se foi passado o argumento do arquivo de saida
-                strcpy(pathArquivoSaida, argv[3]);
-            }
-            else {
-                strcpy(pathArquivoSaida, argv[2]);
-                strcat(pathArquivoSaida, ".out");
-            }
-
-            saidas = simula(circuto1, entradas);
-
-            if(saidas)
-                printf("Simulacao concluida com saidas geradas.\n");
-
-            free(entradas);
-
-            waveout = fopen(pathArquivoSaida, "w");
-
-            if(!waveout) {
-                printf("Erro ao tentar abrir arquivo de saida '%s' para gravacao.\n", pathArquivoSaida);
-            }
-            else {
-                salvarSinais(saidas, waveout);
-                fclose(waveout);
-                printf("Arquivo de saida salvo em '%s'.\n", pathArquivoSaida);
-                free(saidas);
-            }
-        }
-        else {
-            printf("Nao ha entradas para a simulacao do circuito.\n");
-        }
+    // caso onde apenas o circuito, sem sinais de entradas, foi fornecido
+    if(argc == 2) {
+        printf("Para haver simulacao, um arquivo de entrada deve ser fornecido.\n");
+        exit(0);
     }
+
+    // foi fornecido um path para arquivo de entradas da simulacao (argc > 2)
+    f_wave_in = fopen(argv[2], "r");
+
+    if(!f_wave_in) {
+        printf("Impossibilitado de abrir o arquivo de entrada: %s\n", argv[2]);
+        exit(1);
+    }
+
+    printf("Abrindo o arquivo de entrada: %s\n", argv[2]);
+
+    entradas = carregaEntradas(f_wave_in);
+
+    fclose(f_wave_in);
+
+    if(!entradas) {
+        printf("Nao ha entradas para a simulacao do circuito.\n");
+        exit(1);
+    }
+
+    // se foi fornecido o argumento com path para arquivo de saida
+    if(argc > 3) {
+        strcpy(str_wave_out_filepath, argv[3]);
+    }
+    else {
+        // senao, deriva-se do arquivo de entrada
+        strcpy(str_wave_out_filepath, argv[2]);
+        strcat(str_wave_out_filepath, ".out");
+    }
+
+    saidas = simula(circuto1, entradas);
+
+    if(saidas) {
+        printf("Simulacao concluida com saidas geradas.\n");
+    }
+
+    free(entradas);
+
+    f_wave_out = fopen(str_wave_out_filepath, "w");
+
+    if(!f_wave_out) {
+        printf("Erro ao tentar abrir arquivo de saida '%s' para gravacao.\n",
+               str_wave_out_filepath);
+        exit(1);
+    }
+    
+    salvarSinais(saidas, f_wave_out);
+    fclose(f_wave_out);
+    printf("Arquivo de saida salvo em '%s'.\n", str_wave_out_filepath);
+    free(saidas);
 
     return 0;
 }
