d5ff02839f5128d46df8e43020117f43807fc4a3
Tiago Matos <tiagoms88@gmail.com>
2025-06-26 05:48:16 -0300

    allow reading Z values from input files

--- inout.c
+++ inout.c
@@ -24,29 +24,33 @@ Sinais* erroFatalArquivoCorrompido()
 
 Sinais* carregaEntradas(FILE* arquivo)
 {
-    int indice = -1; // indexador do vetor de sinais de entrada
-    ValorLogico valorLogico;
-    Tempo duracao;
-    Sinais* entradas = new_signal_list();
-    Token* it = NULL;
+    int index; // for indexing the array in the input signal list
+    Token* it; // token iterator
+    ValorLogico logic_value;
+    Tempo time_length;
 
-    ListaToken* nomesUsados = novaListaToken(); // nomes de entrada ja lidos
+    Sinais* list_input = new_signal_list();
+
+    // token list to keep the names of read input identifiers
+    ListaToken* list_used_identifiers = novaListaToken();
 
     ListaToken* tokens = tokeniza(arquivo);
 
-    if(!tokens)
+    if (!tokens)
         return NULL;
 
     it = tokens->primeiro;
 
-    if(!it) {
+    if (!it) {
         if (!global_silent_mode)
             printf("%s", MSG_ARQUIVO_ENTRADA_VAZIO);
 
         return NULL;
     }
 
-    // loop para o conjunto de sinais
+    index = -1;
+
+    // loop to read the set of all signal in the file
     while (1)
     {
         if ( isSimbolo(it->valor[0]) )
@@ -54,132 +58,145 @@ Sinais* carregaEntradas(FILE* arquivo)
 
         // TODO: check signal name duplication errors
 
-        insereTokenString(nomesUsados, it->valor, it->linha, it->coluna);
+        insereTokenString(list_used_identifiers,
+                          it->valor,
+                          it->linha,
+                          it->coluna);
 
-        add_new_signal(entradas, it->valor);
-        indice++;
+        add_new_signal(list_input, it->valor);
 
-        if (!avanca(&it))
+        index++;
+
+        if ( !avanca(&it) )
             return erroFatalArquivoCorrompido();
 
         if ( !iguais(it->valor, "{") )
             return erroFatalArquivoCorrompido();
 
-        // loop para um sinal
+        // loop to read one signal
         while (1)
         {
-            if (!avanca(&it))
+            if ( !avanca(&it) )
                 return erroFatalArquivoCorrompido();
 
-            valorLogico = VAL_BLANK;
+            logic_value = VAL_BLANK;
 
-            if( iguais(it->valor, "0") ) {
-                valorLogico = VAL_0;
+            if ( iguais(it->valor, "0") ) {
+                logic_value = VAL_0;
             }
-            else if( iguais(it->valor, "1") ) {
-                valorLogico = VAL_1;
+            else if ( iguais(it->valor, "1") ) {
+                logic_value = VAL_1;
             }
-            else if( iguais(it->valor, "x") || iguais(it->valor, "X") ) {
-                valorLogico = VAL_X;
+            else if ( iguais(it->valor, "x") || iguais(it->valor, "X") ) {
+                logic_value = VAL_X;
             }
-            else if( iguais(it->valor, "}") ) {
+            else if ( iguais(it->valor, "z") || iguais(it->valor, "Z") ) {
+                logic_value = VAL_Z;
+            }
+            else if ( iguais(it->valor, "}") ) {
                 break;
             }
             else {
                 return erroFatalArquivoCorrompido();
             }
 
-            if(!avanca(&it))
+            if ( !avanca(&it) )
                 return erroFatalArquivoCorrompido();
 
-            if( !iguais(it->valor, "(") )
+            if ( !iguais(it->valor, "(") )
                 return erroFatalArquivoCorrompido();
 
-            if(!avanca(&it))
+            if ( !avanca(&it) )
                 return erroFatalArquivoCorrompido();
 
-            if( !isNumNaturalValido(it->valor) )
+            if ( !isNumNaturalValido(it->valor) )
                 return erroFatalArquivoCorrompido();
             
-            duracao = strtol(it->valor, NULL, 10);
-            add_new_pulse((entradas->lista + indice), valorLogico, duracao);
+            time_length = strtol(it->valor, NULL, 10);
 
-            if(!avanca(&it))
+            add_new_pulse( (list_input->lista + index),
+                           logic_value,
+                           time_length );
+
+            if ( !avanca(&it) )
                 return erroFatalArquivoCorrompido();
 
-            if( !iguais(it->valor, ")") )
+            if ( !iguais(it->valor, ")") )
                 return erroFatalArquivoCorrompido();
 
-            if(!avanca(&it))
+            if ( !avanca(&it) )
                 return erroFatalArquivoCorrompido();
 
-            if( iguais(it->valor, ",") )
+            if ( iguais(it->valor, ",") ) {
                 continue;
-            else if( iguais(it->valor, "}") )
+            }
+            else if ( iguais(it->valor, "}") ) {
                 break;
-             
+            }             
         }
 
-        if(!avanca(&it)) {
-            if (!global_silent_mode)
+        if ( !avanca(&it) ) {
+            if ( !global_silent_mode ) {
                 printf("%s", MSG_ARQUIVO_ENTRADA_LIDO_COMPLETO);
+            }
 
             break;
         }
     }
 
-    delete_lista_token(nomesUsados);
+    delete_lista_token(list_used_identifiers);
     delete_lista_token(tokens);
 
-    return entradas;
+    return list_input;
 }
 
 void salvarSinais(Sinais* sinaisSaida, FILE* arqSaida)
 {
     int si; // indexador dos sinais na lista de sinais de entrada
-    Sinal* itSinais = NULL; // Iterador para os sinais num conjunto de entrada ou saida
+    Sinal* it_signal = NULL; // Iterador para os sinais num conjunto de entrada ou saida
     Pulso* it = NULL; // Iterador para os pulsos em um Sinal
 
-    if(!sinaisSaida || !arqSaida)
+    if ( !sinaisSaida || !arqSaida ) {
         return;
+    }
 
     si = 0;
-    itSinais = sinaisSaida->lista;
+    it_signal = sinaisSaida->lista;
 
-    while( si < sinaisSaida->quantidade )
+    while ( si < sinaisSaida->quantidade )
     {
-        fprintf(arqSaida, "%s {", itSinais[si].nome);
+        fprintf(arqSaida, "%s {", it_signal[si].nome);
 
-        it = itSinais[si].pulsos; // Aqui, o indice 0 indica qual dos sinais na lista
+        it = it_signal[si].pulsos; // Aqui, o indice 0 indica qual dos sinais na lista
 
-        while( it->valor != VAL_BLANK )
+        while ( it->valor != VAL_BLANK )
         {
             // Insere virgula apenas se nao eh a primeira iteracao
-            if( it != itSinais[si].pulsos )
+            if ( it != it_signal[si].pulsos )
                 fprintf(arqSaida, ", ");
 
-            switch(it->valor)
+            switch (it->valor)
             {
-                case VAL_1:
-                    fprintf(arqSaida, "1(%llu)", it->tempo);
-                    break;
-                case VAL_0:
-                    fprintf(arqSaida, "0(%llu)", it->tempo);
-                    break;
-                case VAL_X:
-                    fprintf(arqSaida, "x(%llu)", it->tempo);
-                    break;
-                case VAL_Z:
-                    fprintf(arqSaida, "z(%llu)", it->tempo);
-                    break;
-                case VAL_H:
-                    fprintf(arqSaida, "1(%llu)", it->tempo);
-                    break;
-                case VAL_L:
-                    fprintf(arqSaida, "0(%llu)", it->tempo);
-                    break;
-                case VAL_BLANK:
-                    break;
+            case VAL_1:
+                fprintf(arqSaida, "1(%llu)", it->tempo);
+                break;
+            case VAL_0:
+                fprintf(arqSaida, "0(%llu)", it->tempo);
+                break;
+            case VAL_X:
+                fprintf(arqSaida, "x(%llu)", it->tempo);
+                break;
+            case VAL_Z:
+                fprintf(arqSaida, "z(%llu)", it->tempo);
+                break;
+            case VAL_H:
+                fprintf(arqSaida, "1(%llu)", it->tempo);
+                break;
+            case VAL_L:
+                fprintf(arqSaida, "0(%llu)", it->tempo);
+                break;
+            case VAL_BLANK:
+                break;
             }
 
             it++;
